c843dfd34572917912ffcd417f4fc169905b86b8
Evelyn Cho <yehyeoncho@gmail.com>
2024-07-27 20:34:18 -0700

    loading step added

--- frontend/src/healthAdvisor.js
+++ frontend/src/healthAdvisor.js
@@ -4,13 +4,18 @@ import axios from 'axios';
 function HealthAdvisor() {
   const [prompt, setPrompt] = useState('');
   const [response, setResponse] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleResponse = async () => {
+    setIsLoading(true);
     try {
       const result = await axios.post('http://localhost:8000/medChatBot', { prompt });
       setResponse(result.data.response);
     } catch (error) {
       console.error('Error generating response', error);
+    } finally {
+      // after try and catch
+      setIsLoading(false);
     }
   };
 
@@ -26,7 +31,7 @@ function HealthAdvisor() {
       ></textarea>
       <br/>
       <button class="button-4" onClick={handleResponse}>Get Response</button>
-      <p>Response: {response}</p>
+      <p style={{marginLeft: 150, marginRight: 150}}>{isLoading ? 'Loading...' : response}</p>
     </div>
   );
 }
